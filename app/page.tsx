2487ba2540140c1587f6748da12bf9d3ddd662a9
nurRiyad <nur.riyad@fieldnation.com>
2025-12-24 23:11:24 +0600

    add basic content
    Signed-off-by: nurRiyad <nur.riyad@fieldnation.com>

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the Mugdhos Blog</h1>
+    <div className="flex flex-col justify-center items-center h-screen spa">
+      <div className="text-center mb-8">
+      <h1 className="text-3xl font-bold">Welcome to the Mugdhos Blog</h1>
+      <p>Your go-to place for insightful articles and updates.</p>
+      </div>
+      <Image
+        src="/image.png"
+        alt="Blog Image"
+        width={600}
+        height={400}
+      />
     </div>
   );
 }
